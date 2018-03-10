3e1cc99912f93647d10de76ef2cb652ea64efe6a
JacobMowbrayJones <35005363+JacobMowbrayJones@users.noreply.github.com>
2018-03-11 03:29:49 +0800

    Testing Canvas

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
+    console.log("in ready");
 
-    console.log("im in jQuery do ready");
 });
 
 
 function animatePet() {
-    console.log(" in animatePet");
 
-   
+    //create img
     var img = document.createElement('img');
     img.id = "imgForAnimation"
     document.getElementById('petImageDiv').appendChild(img)
@@ -17,9 +16,36 @@ function animatePet() {
     var pics = ["images/picture.png", "images/picture2.png", "images/picture3.png"];
     var el = document.getElementById('imgForAnimation');
 
+
     function toggle() {
         el.src = pics[i]; // set the image
         i = (i + 1) % pics.length; // update the counter
+
+
     }
     setInterval(toggle, 200);
+}
+
+function canvasTest() {
+
+    console.log("in canvas test");
+
+    var ctx = document.getElementById('canvasTest');
+
+    if (ctx.getContext) {
+        ctx = ctx.getContext('2d');
+        var imgForCanvas = new Image();
+
+        imgForCanvas.width = "50px";
+        imgForCanvas.height = "50px";
+        imgForCanvas.onload = function () {
+            ctx.drawImage(imgForCanvas, 100, 100);
+         console.log(" in loaded function");
+
+        }
+        imgForCanvas.src = "images/picture.png"
+
+    }
+
+
 }
