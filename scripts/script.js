1fd2c805ec14d837d9df82bb0c2460ad4457b90d
JacobMowbrayJones <35005363+JacobMowbrayJones@users.noreply.github.com>
2018-03-11 04:03:40 +0800

    Removed old animation stuff
    Animation just done with canvas

--- scripts/script.js
+++ scripts/script.js
@@ -4,49 +4,25 @@ $(document).ready(function () {
 });
 
 
-function animatePet() {
-
-    //create img
-    var img = document.createElement('img');
-    img.id = "imgForAnimation"
-    document.getElementById('petImageDiv').appendChild(img)
+function canvasTest() {
+    var canvasTest = document.getElementById("canvasTest");
+    var ctx = canvasTest.getContext("2d");
+    var imgForCanvas = new Image();
 
     //loop through Images 
     var i = 0;
     var pics = ["images/picture.png", "images/picture2.png", "images/picture3.png"];
-    var el = document.getElementById('imgForAnimation');
-
 
     function toggle() {
-        el.src = pics[i]; // set the image
-        i = (i + 1) % pics.length; // update the counter
+        imgForCanvas.src = pics[i]; 
+        imgForCanvas.onload = function () {
+            ctx.drawImage(imgForCanvas, 0, 0, imgForCanvas.width, imgForCanvas.height,
+                0, 0, canvasTest.width, canvasTest.height);
+        }
+        i = (i + 1) % pics.length; 
 
 
     }
     setInterval(toggle, 200);
-}
-
-function canvasTest() {
-
-    console.log("in canvas test");
-
-    var canvasTest = document.getElementById("canvasTest");
-    var ctx=canvasTest.getContext("2d");
-    
-    
-    var imgForCanvas = new Image();
-
-
-    imgForCanvas.onload = function () {
-
-        ctx.drawImage(imgForCanvas, 0, 0, imgForCanvas.width, imgForCanvas.height,
-            0, 0, canvasTest.width, canvasTest.height);
-        console.log(" in loaded function");
-
-    }
-    imgForCanvas.src = "images/picture.png"
 
 }
-
-   
- 
