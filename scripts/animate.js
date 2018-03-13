10c89ba24feac14659196ee42be6511d3baf2fd6
JacobMowbrayJones <35005363+JacobMowbrayJones@users.noreply.github.com>
2018-03-13 22:54:01 +0800

    Added new idle head turn animations

--- scripts/animate.js
+++ scripts/animate.js
@@ -2,6 +2,12 @@ var idleBlinkImages = ["images/idle1.png", "images/idle1.png", "images/idle1.png
 
 var idleImages = ["images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png"];
 
+var headTurnRightImages = ["images/idle1.png", "images/headturnright1.png", "images/headturnright2.png", "images/headturnright3.png", "images/headturnright3.png","images/headturnright2.png", "images/headturnright1.png", "images/idle1.png"];
+
+
+
+var eyesLookLeftImages = ["images/idle1.png", "images/eyeslookleft1.png", "images/eyeslookleft2.png", "images/eyeslookleft3.png", "images/eyeslookleft3.png","images/eyeslookleft2.png", "images/eyeslookleft1.png", "images/idle1.png"];
+
 var animationIndex = 0;
 var currentImages = idleImages;
 $(document).ready(function () {
@@ -25,15 +31,20 @@ function animate() {
     }
     setInterval(toggleImages, 100);
 }
-function pickNewAnimation()
-{
-  var randomIndex = Math.floor(Math.random() * 5);
+
+function pickNewAnimation() {
+    var randomIndex = Math.floor(Math.random() * 12);
     console.log(randomIndex);
-    if(randomIndex == 1)
-        {
-            currentImages = idleBlinkImages;
-        }
-    else{
+    if (randomIndex == 1 || randomIndex == 2) {
+        currentImages = idleBlinkImages;
+    } else if (randomIndex == 3) {
+
+        currentImages = headTurnRightImages;
+        
+    }else if (randomIndex == 4) {
+
+        currentImages = eyesLookLeftImages;
+    } else {
         currentImages = idleImages;
     }
 }
