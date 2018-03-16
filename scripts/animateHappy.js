4a69b64f3445a62ffcdef26a6de98731b03836b2
JacobMowbrayJones <35005363+JacobMowbrayJones@users.noreply.github.com>
2018-03-17 00:46:35 +0800

    Switches from happy to idle based on new system

--- scripts/animateHappy.js
+++ scripts/animateHappy.js
@@ -1,6 +1,6 @@
-var happyBlinkImages = ["images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/blink2.png", "images/blink3.png", "images/blink2.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png"];
+var happyBlinkImages = ["images/happy1.png", "images/happy2.png", "images/happy3.png", "images/happy4.png", "images/happy3.png", "images/happy2.png", "images/happy1.png", "images/happy5.png", "images/happy6.png", "images/happy7.png", "images/happy6.png", "images/happy5.png", "images/happy1.png", "images/happy1.png", "images/happy1.png", "images/happy1.png"];
 
-var happyImages = ["images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png", "images/idle1.png"];
+var happyImages = ["images/happy1.png", "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png", "images/happy1.png",  "images/happy1.png", "images/happy1.png",  "images/happy1.png",  "images/happy1.png",  "images/happy1.png"];
 
 
 
@@ -15,10 +15,12 @@ function pickNewHappyAnimation() {
     
     var randomIndex = Math.floor(Math.random() * 2 + 1);
     if (randomIndex == 1) {
+        console.log('happyImages');
         currentImages = happyImages;
 
     } else if (randomIndex == 2) {
 
         currentImages = happyBlinkImages;
+        console.log('happyBlinkImages');
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,64 @@
 var State = {
-   Idle: 1,
-   Happy: 2,
+    Idle: 1,
+    Happy: 2,
 };
-var state = State.Idle;
-
+var state = State.Happy;
+var i = 0;
+var currentImages;
+var happiness = 100;
 $(document).ready(function () {
-   currentImages = idleImages;
-    animate();
-    
+    currentImages = idleImages;
+    Update();
+
 })
 
-function Update()
-{
-     function actOnState() {
+function Update() {
+    function actOnState() {
 
-switch(state){
-    case State.Idle:
-        break;
-    case State.Happy:
-        break;
-}
-    }
-    setInterval(actOnState, 100);
-    
-}
-
-function animate() {
-    var i = 0;
-    function toggleImages() {
-        document.getElementById("animationImage").src = currentImages[i];
+        switch (state) {
+            case State.Idle:
+                 console.log('state is idle');
+                happiness = happiness -.5;
+                break;
+            case State.Happy:
+                console.log('state is happy');
+                happiness = happiness -.5;
+                break;
+        }
+        
+        console.log('happiness' + happiness);
         i = (i + 1);
-
-
         if (i == 15) {
             
-            //The pickNew functions randomly switch between
-            //possible of their given state
-            switch(state){
-                case State.Idle:
-                    pickNewIdleAnimation();
-                    break;
-                case State.Happy:
-                    pickNewHappyAnimation();
-                    break;
-            }
-            i = 0;
+          
+             checkState();
+             document.getElementById("animationImage").src = currentImages[i];
+             i = 0;
+        } else {
+            
+             document.getElementById("animationImage").src = currentImages[i];
         }
-
+        
+       
     }
-    setInterval(toggleImages, 100);
+    setInterval(actOnState, 100);
+
 }
+
+//checkState switches states and selects variations
+//of appropriate animation
+function checkState() {
+
+    if(happiness>=50)
+        {
+            state = State.Happy;
+            pickNewHappyAnimation();
+        }
+    else if (happiness<50)
+        {
+            state = State.Idle;
+            pickNewIdleAnimation();
+        }
+}
+
+
